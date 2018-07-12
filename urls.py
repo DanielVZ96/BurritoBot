7c77a5ba9c71d62af9c6d7f42609a70898a78674
Daniel Valenzuela <dsvalenzuela@uc.cl>
2018-07-12 02:34:08 +0000

    some edits for server deployment

--- urls.py
+++ urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('commands/edit', views.edit_commands, name='edit_commands'),
     path('commands/new', views.new_commands, name='new_commands'),
     path('commands/', views.commands, name='commands'),
+    path('', views.login_view, name='login'),
 ]
